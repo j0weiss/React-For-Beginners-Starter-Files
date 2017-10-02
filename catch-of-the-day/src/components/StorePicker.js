3b777e13cfc86c67844f7c7912970eaa6b8b7856
j0weiss <weiss_johannes@icloud.com>
2017-10-02 19:29:24 +0200

    Fetching values from html elements and route to another url when clicking on a button

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
-	render() {
-		return (
-            <form className="store-selector">
-              <h2>Please Enter A Store</h2>
-              <input type="text" required placeholder="Store Name" />
-              <button type="submit">Visit Store -></button>
+    goToStore(event) {
+        event.preventDefault();
+        const storeId = this.storeInput.value;
+
+        this.context.router.transitionTo(`/store/${storeId}`);
+    }
+
+    render() {
+        return (
+            <form className="store-selector" onSubmit={ this.goToStore.bind(this) }>
+            { /* <form className="store-selector" onSubmit={ (event) => this.goToStore(event) }> <- this is syntactically equal to the line above */ }
+                <h2>Please Enter A Store</h2>
+                <input type="text" required placeholder="Store Name" defaultValue={ getFunName() } ref={ (input) => { this.storeInput = input } } />
+                <button type="submit">Visit Store -></button>
             </form>
-		)
-	}
+        )
+    }
 }
+
+// Surface the router using context
+StorePicker.contextTypes = {
+    router: React.PropTypes.object
+};
 
 export default StorePicker;
