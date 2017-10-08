a96024eedf7a285026bc06f7311f14cb2fe74b63
j0weiss <weiss_johannes@icloud.com>
2017-10-08 11:32:26 +0200

    Persist state with Firebase

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyD5eelrrDlO2TiaxloArs_QaHEQDXOlJdY",
+    authDomain: "cotd-f888a.firebaseapp.com",
+    databaseURL: "https://cotd-f888a.firebaseio.com",
+});
+
+export default base;
